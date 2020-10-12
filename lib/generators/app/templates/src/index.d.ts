a11fd9045ae652de1545f934e71ce8253e79b3fd
Gerald <gera2ld@163.com>
2020-10-12 21:02:17 +0800

    fix: d.ts for rollup-plugin-postcss output

--- lib/generators/app/templates/src/index.d.ts
+++ lib/generators/app/templates/src/index.d.ts
@@ -1,6 +1,21 @@
-declare module '*.css' {
-  export const css: string;
-  export const classMap: {
+declare module '*.module.css' {
+  /**
+   * Generated CSS for CSS modules
+   */
+  export const stylesheet: string;
+  /**
+   * Exported classes
+   */
+  const classMap: {
     [key: string]: string;
   };
+  export default classMap;
+}
+
+declare module '*.css' {
+  /**
+   * Generated CSS
+   */
+  const css: string;
+  export default css;
 }
