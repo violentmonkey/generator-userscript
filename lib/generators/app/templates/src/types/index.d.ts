d80f257d3c7dc7df23623528ea6a079479cc4566
Gerald <gera2ld@live.com>
2021-11-30 22:44:49 +0800

    fix: fix JSX return type
    fix #5

--- lib/generators/app/templates/src/types/index.d.ts
+++ lib/generators/app/templates/src/types/index.d.ts
@@ -1,6 +1,11 @@
 import * as dom from '@violentmonkey/dom';
 import * as ui from '@violentmonkey/ui';
+import { VChildren, DomNode } from '@gera2ld/jsx-dom';
 
 declare global {
   const VM: typeof dom & typeof ui;
+
+  namespace JSX {
+    type Element = VChildren; // Change to DomNode if jsxFactory is set to VM.hm
+  }
 }
